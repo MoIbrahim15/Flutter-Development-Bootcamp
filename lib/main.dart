dc77aa5660d069d6db87b8bb625f08e75bbec5a8
MohamedIbrahim <mohamed_ibrahim15@hotmail.com>
2020-05-21 05:00:19 +0200

    dices

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,76 +8,55 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage("images/logo.jpg"),
-              ),
-              Text(
-                'Mohamed',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Senior Android Developer",
-                style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20,
-                    color: Colors.teal.shade100,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-                width: 150,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "+201014776617",
-                      style: TextStyle(
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 14,
-                        color: Colors.teal.shade900,
-                      ),
-                    )),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "mohamed_ibrahim15@hotmail.com",
-                      style: TextStyle(
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 14,
-                        color: Colors.teal.shade900,
-                      ),
-                    )),
-              ),
-            ],
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red,
           ),
-        ),
+          body: DiceAPP()),
+    );
+  }
+}
+
+class DiceAPP extends StatefulWidget {
+  @override
+  _DiceAPPState createState() => _DiceAPPState();
+}
+
+class _DiceAPPState extends State<DiceAPP> {
+  int rightDiceNumber = 2;
+  int leftDiceNumber = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDices();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDices();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  void changeDices() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
