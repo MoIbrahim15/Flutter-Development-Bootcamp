650ee4fb2242a49dca29a47afcd6f7d852cc4456
MohamedIbrahim <mohamed_ibrahim15@hotmail.com>
2020-05-28 11:59:42 +0200

    starter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Quizzler());
 
-void playSound(int soundID) {
-  final player = AudioCache();
-  player.play('note$soundID.wav');
-}
-
-Expanded buildKey({int soundID, Color color}) {
-  return Expanded(
-    child: FlatButton(
-      color: color,
-      onPressed: () {
-        playSound(soundID);
-      },
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            buildKey(soundID: 1, color: Colors.red),
-            buildKey(soundID: 2, color: Colors.orange),
-            buildKey(soundID: 3, color: Colors.yellow),
-            buildKey(soundID: 4, color: Colors.green),
-            buildKey(soundID: 5, color: Colors.teal),
-            buildKey(soundID: 6, color: Colors.blue),
-            buildKey(soundID: 7, color: Colors.purple)
-          ],
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
         ),
       ),
-    ));
+    );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
